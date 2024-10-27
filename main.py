99ce82a8c49a5878c190dfc2be02fb00d4e94d5b
Nitin Chauhan <44166323+buntynitin@users.noreply.github.com>
2024-10-27 22:52:52 +0530

    Added start time

--- main.py
+++ main.py
@@ -10,21 +10,23 @@ db = client["analytics"]
 collection = db["analytics"]
 ist = pytz.timezone("Asia/Kolkata")
 
-# Function to process MongoDB documents
 def process_documents(docs):
     processed_data = []
     for doc in docs:
         current_time = datetime.fromtimestamp(doc['currentTimestamp'] / 1000).astimezone(ist).strftime("%d-%m-%Y %I:%M %p")
-        usage_stats = [
-            {
-                "packageName": stat['packageName'],
-                "totalTimeInForeground": stat['totalTimeInForeground'] / 60000,  # Convert to minutes
-                "lastTimeStamp": datetime.fromtimestamp(stat['lastTimeStamp'] / 1000).astimezone(ist).strftime("%I:%M %p")
-            }
-            for stat in doc['usageStats'] if stat['totalTimeInForeground'] > 0
-        ]
-        
-        # Only add documents with relevant usage stats
+        usage_stats = sorted(
+            [
+                {
+                    "packageName": stat['packageName'],
+                    "totalTimeInForeground": stat['totalTimeInForeground'] / 60000,
+                    "firstTimeStamp": datetime.fromtimestamp(stat['firstTimeStamp'] / 1000).astimezone(ist).strftime("%d-%m-%Y %I:%M %p"), 
+                    "lastTimeStamp": datetime.fromtimestamp(stat['lastTimeStamp'] / 1000).astimezone(ist).strftime("%d-%m-%Y %I:%M %p"),  
+                }
+                for stat in doc['usageStats'] if stat['totalTimeInForeground'] > 0
+            ],
+            key=lambda x: x['totalTimeInForeground'],
+            reverse=True
+        )
         if usage_stats:
             processed_data.append({
                 "current_time": current_time,
@@ -43,7 +45,7 @@ for doc in data:
     if doc["current_time"] == selected_time:
         st.write(f"### Usage Stats for {selected_time}")
         df = pd.DataFrame(doc["usage_stats"])
-        st.write(df[['packageName', 'totalTimeInForeground', 'lastTimeStamp']])
+        st.write(df[['packageName', 'totalTimeInForeground', 'firstTimeStamp', 'lastTimeStamp']])
         st.write("#### Total Time in Foreground (minutes) per App")
         chart_data = df.set_index("packageName")["totalTimeInForeground"]
         st.bar_chart(chart_data)
